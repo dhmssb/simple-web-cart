716b5c83b9b73f46cdbd86a9ea08715e6f9c5af1
dhmssb <dhmssb@192.168.1.13>
2023-10-20 09:43:52 +0700

    change password

--- app/database/fakers/user_faker.go
+++ app/database/fakers/user_faker.go
@@ -14,7 +14,7 @@ func UserFaker(db *gorm.DB) *models.User {
 		ID:        uuid.New().String(),
 		Name:      faker.Name(),
 		Username:  faker.Username(),
-		Password:  "password",
+		Password:  "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
 		CreatedAt: time.Time{},
 		UpdatedAt: time.Time{},
 		DeletedAt: gorm.DeletedAt{},
